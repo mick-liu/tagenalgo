60e22c7e8994a0381136a594484117815056ea69
Mick Liu <31812232+mick-liu@users.noreply.github.com>
2020-10-24 00:35:11 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tagenalgo",
+    version="1.0.2",
+    author="Mick Liu",
+    author_email="mickey70636@gmail.com",
+    description="A parameter optimization algorithm for trading strategy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mick-liu/tagenalgo",
+    license='BSD',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+)
